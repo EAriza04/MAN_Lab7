903576e9e80cc2c6a5ccdaa58cd475b11c1e8da1
Enri238 <enrihomipat@icloud.com>
2025-05-22 20:50:33 +0200

    Smoke Test

---
+++ smoke-test.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { sleep, check, fail } from 'k6';
+
+export const options = {
+    vus: 5,
+    duration: '1m',
+    thresholds: {
+        'http_req_failed': [
+            {
+                threshold: 'rate==0',
+                abortOnFail: true
+            }
+        ],
+        'http_req_duration': [
+            {
+                threshold: 'avg<500',
+                abortOnFail: true
+            }
+        ]
+    },
+};
+
+export default () => {
+    const res = http.get('http://localhost:8080/medico/1');
+
+    check(res, {
+        'response code was 200': (res) => res.status === 200,
+    });
+
+    sleep(1);
+};
