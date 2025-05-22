1772ad35e62c1939d473386c23995c9d0421036b
Enri238 <enrihomipat@icloud.com>
2025-05-22 20:57:57 +0200

    format

--- smoke-test.js
+++ smoke-test.js
@@ -5,13 +5,13 @@ export const options = {
     vus: 5,
     duration: '1m',
     thresholds: {
-        'http_req_failed': [
+        http_req_failed: [
             {
                 threshold: 'rate==0',
                 abortOnFail: true
             }
         ],
-        'http_req_duration': [
+        http_req_duration: [
             {
                 threshold: 'avg<500',
                 abortOnFail: true
